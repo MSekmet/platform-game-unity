dbd84286926a155f124d265950519d9c0d744d51
İlter Fehmi Ataulusoy <MSekmet@gmail.com>
2026-01-26 14:49:11 +0300

    Update Main Menu design and logic

--- Assets/Scripts/MainMenuControl.cs
+++ Assets/Scripts/MainMenuControl.cs
@@ -1,8 +1,14 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuControl : MonoBehaviour
 {
+
+    [SerializeField] AudioSource ButtonPress;
+    [SerializeField] GameObject fadeOut;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,6 +23,21 @@ public class MainMenuControl : MonoBehaviour
 
     public void StartGame()
     {
+        
+        ButtonPress.Play();
+        fadeOut.SetActive(true);
+        StartCoroutine(PlayTheGame());
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+    IEnumerator PlayTheGame()
+    {
+        yield return new WaitForSeconds(2);
         SceneManager.LoadScene(4);
     }
 }
